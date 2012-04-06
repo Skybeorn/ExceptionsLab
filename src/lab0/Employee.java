b72509228d3bf8921600e9b2df9699830e987151
Skybeorn <Skybeorn@10.30.16.54>
2012-04-06 12:06:21 -0500

    Did Lab 0. Looked over Exception Handling. Noticed it is usually on he Setters.

--- src/lab0/Employee.java
+++ src/lab0/Employee.java
@@ -2,23 +2,8 @@ package lab0;
 
 import java.util.Date;
 
-/**
- * In this challenge you need to address the probability that at some
- * point the arguments to method parameters may not be valid. 
- * <p>
- * For example, String arguments may be null or empty; other objects may be 
- * null; or primitive numbers may be out of acceptable range.
- * <p>
- * You need to validate ALL method parameters to make sure any and all 
- * arguments are valid. The only exception is when any argument is acceptable 
- * based on requirements. Fix the code below using if logic to validate
- * method arguments and throw IllegalArgumentException or a custom
- * exception if the validation fails.
- * 
- * @author  Jim Lombardo, jlombardo@wctc.edu
- * @version 1.00
- */
 public class Employee {
+
     public static final int MAX_VACATION_DAYS = 28;
     private String firstName;
     private String lastName;
@@ -35,19 +20,21 @@ public class Employee {
     }
 
     public Employee(String firstName, String lastName, String ssn, Date hireDate, int daysVacation) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.ssn = ssn;
-        this.hireDate = hireDate;
-        this.daysVacation = daysVacation;
+        setFirstName(firstName);
+        setLastName(lastName);
+        setSsn(ssn);
+        setHireDate(hireDate);
+        setDaysVacation(daysVacation);
     }
-    
+
     public int getDaysVacation() {
         return daysVacation;
     }
 
     public void setDaysVacation(int daysVacation) {
-        this.daysVacation = daysVacation;
+        if (daysVacation < 0 || daysVacation > MAX_VACATION_DAYS) {
+            throw new IllegalArgumentException("Out of Required Range.");
+        }
     }
 
     public String getFirstName() {
@@ -55,7 +42,9 @@ public class Employee {
     }
 
     public void setFirstName(String firstName) {
-        this.firstName = firstName;
+        if (firstName == null || firstName.length() < 2) {
+            throw new IllegalArgumentException("First Name does not Meet Standard.");
+        }
     }
 
     public Date getHireDate() {
@@ -63,7 +52,10 @@ public class Employee {
     }
 
     public void setHireDate(Date hireDate) {
-        this.hireDate = hireDate;
+        Date today = new Date();
+        if (hireDate == null || hireDate.after(today)) {
+            throw new IllegalArgumentException("Impossibile.");
+        }
     }
 
     public String getLastName() {
@@ -71,7 +63,9 @@ public class Employee {
     }
 
     public void setLastName(String lastName) {
-        this.lastName = lastName;
+        if (lastName == null || lastName.length() < 2) {
+            throw new IllegalArgumentException("Last Name does not Meet Standard.");
+        }
     }
 
     public final String getSsn() {
@@ -79,8 +73,9 @@ public class Employee {
     }
 
     public void setSsn(String ssn) {
-        this.ssn = ssn;
+        if (ssn == null || ssn.length() < 9) {
+            throw new IllegalArgumentException("Ssn should only be Numbers "  
+                    + " No Dashes or Spaces");
+        }
     }
-    
-    
 }
